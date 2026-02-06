2f0dca7e4f4b23731c948f4466dd0f0b94f88bbc
Robert Bednarowicz <robb.bednarowicz@gmail.com>
2026-02-06 20:00:20 +0100

    Add Idea and Step factories and configure DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Idea;
+use App\Models\Step;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +18,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            'name' => 'Robert Bednarowicz',
+            'email' => 'robb.bednarowicz@gmail.com',
+            'password' => Hash::make('password'),
+        ]);    
+    
+        User::factory(10)->create();
+
+        Idea::factory(50)->create();
+
+        Step::factory(200)->create();
     }
 }
