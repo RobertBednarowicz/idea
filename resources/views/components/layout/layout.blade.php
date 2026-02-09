4847519aca67e8074fba68234fc00c4dd41f895c
Robert Bednarowicz <robb.bednarowicz@gmail.com>
2026-02-09 21:58:46 +0100

    feat: add flash message notifications for user feedback

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -19,5 +19,17 @@
     <main class="flex-1 flex py-10 overflow-y-auto">
         {{ $slot }}
     </main>
+
+    @session('success')
+        <div
+            class="fixed bottom-6 right-6 px-4 py-2 text-white bg-green-500 rounded-md"
+            x-data="{ show: true }"
+            x-init="setTimeout(() => show = false, 3000)"    
+            x-show="show"
+            x-transition.opacity.duration.300ms"
+        >
+            {{ session('success') }}
+        </div>
+    @endsession
 </body>
 </html>
